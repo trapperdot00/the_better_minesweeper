5a2ca0e75d4d7bbd8e991f3b30ab1beebe59f1ec
Csernus Richárd Benjámin <csernusricsike@gmail.com>
2026-01-30 12:29:00 +0100

    Added forward declaration header.

---
+++ include/FwdDecls.h
@@ -0,0 +1,18 @@
+#ifndef FWDDECLS_H
+#define FWDDECLS_H
+
+struct Point;
+struct Difficulty;
+class AnsiColor;
+
+class Tile;
+class EmptyTile;
+class MineTile;
+class TileFactory;
+
+class Tiles;
+class Cursor;
+class GameContext;
+class Game;
+
+#endif
